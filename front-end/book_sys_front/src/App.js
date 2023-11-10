8b46de96a44242a5d7e5b87ccd09a3372b77a32c
sin6338ki <jyshin38@gmail.com>
2023-11-10 13:04:56 +0900

    feat(FE): 도서 대출 페이지

--- front-end/book_sys_front/src/App.js
+++ front-end/book_sys_front/src/App.js
@@ -4,6 +4,10 @@ import Main from "./components/Main";
 import Menu from "./components/Menu";
 import Login from "./components/member/Login";
 import Join from "./components/member/Join";
+import RentList from "./components/member/RentList";
+import Rent from "./components/Menu/Rent";
+import Return from "./components/Menu/Return";
+import BookList from "./components/Menu/BookList";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route path="/join" element={<Join />} />
         <Route path="/login" element={<Login />} />
         <Route path="/book-sys" element={<Menu />} />
+        <Route path="/rent" element={<Rent />} />
+        <Route path="/return" element={<Return />} />
+        <Route path="/rent-list" element={<RentList />} />
+        <Route path="/book-list" elememt={<BookList />} />
       </Routes>
     </div>
   );
